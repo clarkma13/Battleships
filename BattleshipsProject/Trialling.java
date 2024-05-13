01f3895a51257ee1d8853807d44e2928d4a92388
clarkma13 <168600505+clarkma13@users.noreply.github.com>
2024-05-14 11:55:05 +1200

    Add files via upload
    First commit, worked on trialling different board prints

---
+++ BattleshipsProject/Trialling.java
@@ -0,0 +1,39 @@
+
+/**
+ * Write a description of class Trialling here.
+ *
+ * Max Clarke
+ * 14/5/24
+ */
+public class Trialling
+{
+    public static void main(String[] args){
+        final int WIDTH = 11;
+        final int HEIGHT = 11;
+        String grid[][] = new String[WIDTH][HEIGHT];
+        String [] letters = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J"};
+        //Sets top row of array as numbers
+        grid[0][0] = " ";
+        for(int x=1; x<WIDTH; x++){
+                grid[x][0] = String.valueOf(x);
+        }
+        for(int y=1; y<HEIGHT; y++){
+                grid[0][y] = letters[y-1];
+        }
+        //Fills the rest of the grid with blank
+        for(int y=1; y<HEIGHT; y++){
+            for(int x=1; x<WIDTH; x++){
+                grid[x][y] = "."; 
+            }
+        }
+        //Turn functions
+    
+            //Print grid at start of turn
+        for(int y=0; y<HEIGHT; y++){
+            for(int x=0; x<WIDTH; x++){
+                    System.out.print(grid[x][y]+" "); 
+            }
+            System.out.println("");
+        }
+    }
+}
